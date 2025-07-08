2fb04e26d0f4f37241b459f916b788e6783792ca
EstoesMoises <118999648+EstoesMoises@users.noreply.github.com>
2025-07-08 20:24:28 +0100

    Just testing adding a schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,48 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightOpenAPI, { openAPISidebarGroups } from "starlight-openapi";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			favicon: '/favicon.ico',
-			title: 'Stack Overflow for Teams SDK',
-			logo: {
-				light: './src/assets/stackoverflow-black.svg',
-				dark: './src/assets/stackoverflow-black.svg',
-				replacesTitle: true
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting Started', slug: 'guides/quickstart' },
-						{ label: 'Authentication', slug: 'guides/authentication'},
-						{ label: 'Rate Limits', slug: 'guides/rate-limiting'},
-					],
-				},
-				{
-					label: 'API Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [
+        // Generate the OpenAPI documentation pages.
+        starlightOpenAPI([
+          {
+            base: "api/schema",
+            schema: "./src/schemas/swagger.json",
+			label: "API Schema"
+          },
+        ]),
+      ],
+      favicon: "/favicon.ico",
+      title: "Stack Overflow for Teams SDK",
+      logo: {
+        light: "./src/assets/stackoverflow-black.svg",
+        dark: "./src/assets/stackoverflow-black.svg",
+        replacesTitle: true,
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Getting Started", slug: "guides/quickstart" },
+            { label: "Authentication", slug: "guides/authentication" },
+            { label: "Rate Limits", slug: "guides/rate-limiting" },
+          ],
+        },
+        ...openAPISidebarGroups,
+      ],
+    }),
+  ],
 });
--- src/content/docs/reference/index.md
+++
@@ -1,4 +0,0 @@
----
-title: Endpoints
-description: A reference page in my new Starlight docs site.
----
