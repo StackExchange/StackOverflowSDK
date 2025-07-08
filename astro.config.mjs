37c512e438b876fd287bf056e6ae637efaffa874
EstoesMoises <118999648+EstoesMoises@users.noreply.github.com>
2025-07-08 16:27:02 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,23 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Stack Overflow for Teams SDK',
+			logo: {
+				light: './src/assets/stackoverflow-black.svg',
+				dark: './src/assets/stackoverflow-black.svg',
+				replacesTitle: true
+			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide', slug: 'guides/quickstart' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: 'API Reference',
 					autogenerate: { directory: 'reference' },
 				},
 			],
