5af8570705f15ebeaeeced2385325a21e3e1b19b
EstoesMoises <118999648+EstoesMoises@users.noreply.github.com>
2025-07-08 23:54:30 +0100

    placeholder info

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,31 +4,51 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			favicon: '/favicon.ico',
-			title: 'Stack Overflow for Teams SDK',
-			logo: {
-				light: './src/assets/stackoverflow-black.svg',
-				dark: './src/assets/stackoverflow-black.svg',
-				replacesTitle: true
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting Started', slug: 'guides/quickstart' },
-						{ label: 'Authentication', slug: 'guides/authentication'},
-						{ label: 'Rate Limits', slug: 'guides/rate-limiting'},
-					],
-				},
-				{
-					label: 'API Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+   integrations: [
+   	starlight({
+   		favicon: '/favicon.ico',
+   		title: 'Stack Overflow for Teams SDK',
+   		logo: {
+   			light: './src/assets/stackoverflow-black.svg',
+   			dark: './src/assets/stackoverflow-black.svg',
+   			replacesTitle: true
+   		},
+   		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+   		sidebar: [
+   			{
+   				label: 'Guides',
+   				items: [
+   					{ label: 'Getting Started', slug: 'guides/quickstart' },
+   					{ label: 'Authentication', slug: 'guides/authentication' },
+   					{ label: 'Rate Limits', slug: 'guides/rate-limiting' },
+   				],
+   			},
+   			{
+   				label: 'Questions',
+   				collapsed: true,
+   				items: [
+   					{ label: 'create', slug: 'questions/create' },
+   				]
+   			},
+   			{
+   				label: 'Answers',
+   				collapsed: true,
+   				items: [
+   					{ label: 'list', slug: 'answers/list' },
+   				]
+   			},
+   			{
+   				label: 'Tags',
+   				collapsed: true,
+   				items: [
+   					{ label: 'search', slug: 'tags/search' },
+   				]
+   			},
+   			{
+   				label: 'API Reference',
+   				autogenerate: { directory: 'reference' },
+   			},
+   		],
+   	}),
+   ],
 });
