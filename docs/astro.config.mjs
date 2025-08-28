4ec4a7b52904c92816fbf2f787a1535fa9454a22
EstoesMoises <mgarcia@stackoverflow.com>
2025-08-28 09:40:28 -0400

    added github pages config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://stackexchange.github.io',
+   base: '/StackOverflowSDK',
    integrations: [
    	starlight({
    		favicon: '/favicon.ico',
